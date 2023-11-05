f25753c0733ce5d442b7e3783d7567ea6f0634d0
Rafael <rafael271alves@gmail.com>
2023-11-05 19:38:13 -0300

    file formatted

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     return (
         <section
             id="home"
-            className="h-screenHeightWithoutHeader flex flex-col-reverse items-center justify-center gap-4 py-8 md:flex-row md:justify-between md:py-10"
+            className="flex h-screenHeightWithoutHeader flex-col-reverse items-center justify-center gap-4 py-8 md:flex-row md:justify-between md:py-10"
         >
             <TypingName />
             <Avatar />
