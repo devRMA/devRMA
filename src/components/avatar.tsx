b443da54223c9e321fd1388289e4a1bd58c91983
Rafael <rafael271alves@gmail.com>
2023-11-05 19:36:27 -0300

    added priority to first image

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -11,10 +11,10 @@ export const Avatar = () => {
             className="grid h-40 w-40 auto-rows-auto rounded-full md:h-[25rem] md:w-[25rem]"
         >
             <MouseParallaxChild factorX={0.03} factorY={0.03} className="absolute overflow-hidden blur-[1px]">
-                <Image as={NextImage} width={500} height={500} src="/background.png" alt="Background" />
+                <Image as={NextImage} width={500} height={500} src="/background.png" alt="Background" priority />
             </MouseParallaxChild>
             <MouseParallaxChild factorX={0.01} factorY={0.01} className="z-0">
-                <Image as={NextImage} width={500} height={500} src="/avatar.png" alt="Avatar" />
+                <Image as={NextImage} width={500} height={500} src="/avatar.png" alt="Avatar" priority />
             </MouseParallaxChild>
         </MouseParallaxContainer>
     );
