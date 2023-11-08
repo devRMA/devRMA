20c9f5df888d2929fb5a882054a7ec23122ce226
Rafael <rafael271alves@gmail.com>
2023-11-07 22:08:36 -0300

    added useScroll to stop typing when element is not in the screen

--- src/components/typing-name.tsx
+++ src/components/typing-name.tsx
@@ -1,18 +1,23 @@
 "use client";
 
 import { clsx } from "clsx";
-import { useEffect, useRef } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 import Typed from "typed.js";
 
 import { title } from "@/components/primitives";
 
 export const TypingName = () => {
-    const element = useRef(null);
+    const element = useRef<HTMLElement | null>(null);
+    const typed = useRef<Typed | null>(null);
+    const [isTyping, setIsTyping] = useState(false);
+    const { scrollYProgress } = useScroll({
+        target: element,
+    });
 
     useEffect(() => {
         const color = title({ color: "blue" });
-
-        const typed = new Typed(element.current, {
+        typed.current = new Typed(element.current, {
             strings: [
                 `<span class="${color}">DevRMA</<span>^1000`,
                 `<span class="${color}">Dev</span>eloper<br><span class="${color}">R</span>afael<br><span class="${color}">M</span>artins<br><span class="${color}">A</span>lves^1000`,
@@ -21,11 +26,26 @@ export const TypingName = () => {
             backSpeed: 50,
             loop: true,
         });
+        setIsTyping(true);
 
         return () => {
-            typed.destroy();
+            typed.current?.destroy();
         };
     }, []);
+
+    useMotionValueEvent(scrollYProgress, "change", (latest) => {
+        if (latest <= 0.01) {
+            if (isTyping) {
+                typed.current?.stop();
+                setIsTyping(false);
+            }
+        } else {
+            if (!isTyping) {
+                typed.current?.start();
+                setIsTyping(true);
+            }
+        }
+    });
 
     return (
         <h1 className={clsx("h-[9rem] w-48", title())}>
